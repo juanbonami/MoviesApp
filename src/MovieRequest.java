526c14cebd41c314e388ed07820f60fb20769dd9
Juan Bonami <Juanbonami@gmail.com>
2020-06-15 16:29:02 -0500

    defined url

--- src/MovieRequest.java
+++ src/MovieRequest.java
@@ -1,10 +1,12 @@
 import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MovieRequest {
 
     private static HttpURLConnection connection;
 
     public static void main(String[] args) {
-        
+
+        URL url = new URL("http://www.omdbapi.com/?apikey=8e2e09a4&t=");
     }
 }
