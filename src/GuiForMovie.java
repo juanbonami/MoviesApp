80d2ad5cda0fd3a32028c44a4dad370587fa5f63
Juan Bonami <Juanbonami@gmail.com>
2020-06-15 20:41:22 -0500

    added main method and created a new instance of JFrame and Jlabel and JButton components

--- src/GuiForMovie.java
+++ src/GuiForMovie.java
@@ -12,5 +12,17 @@ public class GuiForMovie {
     JLabel label;
     String textFromTextField;
 
-    
+    public static void main(String[] args) {
+
+        // create a new frame to store text field and button
+        frame = new JFrame("textfield");
+
+        //create a label to display text
+        label = new JLabel("nothing entered");
+
+        // create a new button
+        button = new JButton("submit");
+
+        
+    }
 }
