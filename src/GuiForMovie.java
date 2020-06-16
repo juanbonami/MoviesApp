66032396b0ff1e8702ed4080a2f27de8d351dc4f
Juan Bonami <Juanbonami@gmail.com>
2020-06-15 20:35:05 -0500

    defined some components

--- src/GuiForMovie.java
+++ src/GuiForMovie.java
@@ -1,4 +1,16 @@
+import javax.swing.*;
+
 public class GuiForMovie {
 
     MovieRequest movieRequest = new MovieRequest();
+
+    JTextField textField;
+    JTextArea textArea;
+    JScrollPane scrollPane;
+    JFrame frame;
+    JButton button;
+    JLabel label;
+    String textFromTextField;
+
+    
 }
